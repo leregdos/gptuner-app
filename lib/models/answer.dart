5a6d04ce99eb4a02994751762da7683c34dcfb82
Sonny Mandakhnaran <sm8694@nyu.edu>
2023-10-01 23:50:09 +0400

    Update answer model

--- lib/models/answer.dart
+++ lib/models/answer.dart
@@ -1,12 +1,10 @@
 class Answer {
   late String? uid;
   late String? content;
-  late String? associatedPrompt;
 
-  Answer({this.uid, this.content, this.associatedPrompt});
+  Answer({this.uid, this.content});
   Answer.fromJson(Map<String, dynamic> json) {
     uid = json["_id"] ?? '';
     content = json["content"] ?? '';
-    associatedPrompt = json["associatedPrompt"] ?? '';
   }
 }
