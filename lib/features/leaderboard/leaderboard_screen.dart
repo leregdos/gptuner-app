90bfd7c4c2eb5b53634d30773850cdcdb372e8ad
Sonny Mandakhnaran <sm8694@nyu.edu>
2024-02-25 02:11:42 +0400

    Make some UI changes on leaderboard

--- lib/features/leaderboard/leaderboard_screen.dart
+++ lib/features/leaderboard/leaderboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gptuner/providers/leaderboard_state.dart';
 import 'package:gptuner/shared/utils/functions.dart';
+import 'package:gptuner/shared/widgets/custom_loader.dart';
 import 'package:provider/provider.dart';
 import 'package:gptuner/theme/app_theme.dart';
 
@@ -46,21 +47,66 @@ class _LeaderboardScreenState extends State<LeaderboardScreen>
 
     // Check if items map is not empty to display the data
     if (items.isNotEmpty) {
-      return ListView(
-        children: items.entries.map((entry) {
-          return ListTile(
-            title: Text(entry.key), // User's name
-            trailing: Text(
-              '${entry.value}',
-              style: AppTheme.getTheme().textTheme.bodySmall,
-            ), // Count
-          );
-        }).toList(),
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: ListView.separated(
+                shrinkWrap:
+                    true, // Important to prevent ListView from expanding infinitely
+                physics:
+                    NeverScrollableScrollPhysics(), // Disable scrolling in the ListView
+                itemCount: items.length,
+                separatorBuilder: (context, index) =>
+                    Divider(color: AppTheme.getTheme().dividerColor),
+                itemBuilder: (context, index) {
+                  String key = items.keys.elementAt(index);
+                  int value = items[key]!;
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: AppTheme.getTheme().colorScheme.surface,
+                      child: Text('${index + 1}',
+                          style: TextStyle(
+                              color:
+                                  AppTheme.getTheme().colorScheme.onSurface)),
+                    ),
+                    title: Text(key,
+                        style: AppTheme.getTheme()
+                            .textTheme
+                            .titleMedium), // User's name
+                    trailing: Text('$value',
+                        style:
+                            AppTheme.getTheme().textTheme.bodySmall), // Count
+                  );
+                },
+              ),
+            ),
+            if (category == 'prompts' || category == 'demonstrations')
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Only submissions with at least 2 validations are considered.',
+                  style: AppTheme.getTheme().textTheme.bodySmall,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+          ],
+        ),
       );
     } else {
-      return buildNoAvailableDataMessage(
-          "The leaderboard appears empty at the time. Please check back later.",
-          context);
+      if (leaderboardState.tried) {
+        return buildNoAvailableDataMessage(
+            "The leaderboard appears empty at the time. Please check back later.",
+            context);
+      } else {
+        return Center(
+          child: Container(
+            color: Colors.grey.withOpacity(0.7),
+            child: const CustomLoader(),
+          ),
+        );
+      }
     }
   }
 
@@ -70,7 +116,7 @@ class _LeaderboardScreenState extends State<LeaderboardScreen>
       backgroundColor: AppTheme.getTheme().primaryColor,
       appBar: AppBar(
         backgroundColor: AppTheme.getTheme().colorScheme.background,
-        title: Text('Leaderboard',
+        title: Text('Top 10 Users',
             style: AppTheme.getTheme().textTheme.displaySmall),
         bottom: TabBar(
           onTap: (val) {
@@ -80,17 +126,20 @@ class _LeaderboardScreenState extends State<LeaderboardScreen>
           tabs: [
             Tab(
                 child: Text(
-              'Prompts',
+              'Prompts\nSubmitted',
+              textAlign: TextAlign.center,
               style: AppTheme.getTheme().textTheme.titleMedium,
             )),
             Tab(
                 child: Text(
-              'Answers',
+              'Answers\nSubmitted',
+              textAlign: TextAlign.center,
               style: AppTheme.getTheme().textTheme.titleMedium,
             )),
             Tab(
                 child: Text(
-              'Validations',
+              'Validations\nMade',
+              textAlign: TextAlign.center,
               style: AppTheme.getTheme().textTheme.titleMedium,
             )),
           ],
